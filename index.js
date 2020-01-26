42b527fc2845886d385f4ff0d8cff51cd4f481dc
Paul Perez <colopaul@hotmail.com>
2020-01-26 13:55:08 -0500

    phase 2

--- index.js
+++ index.js
@@ -19,3 +19,23 @@ colorBlock.addEventListener("click", (ev) => {
 	console.dir(ev.target);
 	ev.target.classList.toggle("hover_w");
 });
+
+let gridForm = document.forms[0];
+gridForm.addEventListener("submit", (ev) => {
+	ev.preventDefault();
+	let width = document.querySelector('input[name="width"]').value;
+	let height = document.querySelector('input[name="height"]').value;
+	createGrid(width, height);
+});
+
+const createGrid = (row, cols) => {
+	let gridContainer = document.querySelector(".grid-container");
+	let grid = [];
+	row = row * 1;
+	cols = cols * 1;
+	let multi = row * cols;
+	for (let i = 0; i < row; i++) {
+		gridContainer.innerHTML += `<div id=${i} class="parent"></div>`;
+	}
+	console.log(grid);
+};
